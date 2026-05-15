de0fcb64993f27af255363ca855a3f05247b8175
Aron Dingemanse <aron@dingweb.nl>
2026-05-15 21:09:59 +0200

    Fix LocalInstall Ordering

--- iso-src/install/scripts/LocalInstall.sh
+++ iso-src/install/scripts/LocalInstall.sh
@@ -1,8 +1,8 @@
 set -e
 
-bash ../components/build_and_install_doosan_driver.sh
-sudo bash ../components/install_apt_dependencies.sh
 sudo bash ../components/install_ros2.sh
+sudo bash ../components/install_apt_dependencies.sh
+bash ../components/build_and_install_doosan_driver.sh
 sudo bash ../components/install_vs_code.sh
-sudo bash ../components/practicum_dependencies.sh
 bash ../components/realsense_apt_installation.sh
+sudo bash ../components/practicum_dependencies.sh
