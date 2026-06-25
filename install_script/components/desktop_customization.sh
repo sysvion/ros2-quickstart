605f13d21638c854840cd6fbdfca0bc19c8debf4
Aron Dingemanse <aron@dingweb.nl>
2026-06-25 23:20:27 +0200

    Fix user creator install and desktop wallpaper

--- install_script/components/desktop_customization.sh
+++ install_script/components/desktop_customization.sh
@@ -13,6 +13,20 @@ sudo -A mkdir -p /etc/dconf/db/local.d/
 sudo -A tee  /etc/dconf/db/local.d/00-favorite-apps <<EOF
 [org/gnome/shell]
 favorite-apps = ['firefox_firefox.desktop', 'org.gnome.Terminal.desktop', 'account-creation-tool.desktop', 'org.gnome.Nautilus.desktop','code.desktop', 'ros2-tutorial-gui.desktop', 'start_ursim.desktop', 'ur_moveit.desktop']
+
+[org/gnome/desktop/background]
+
+# Specify the path to the desktop background image file
+picture-uri='file:///opt/install/components/art002e009285~large.jpg'
+
+# Specify one of the rendering options for the background image:
+picture-options='zoom'
+
+# Specify the left or top color when drawing gradients, or the solid color
+primary-color='000000'
+
+# Specify the right or bottom color when drawing gradients
+secondary-color='FFFFFF'
 EOF
 
 sudo -A dconf update
--- install_script/components/user_creator/install.sh
+++ install_script/components/user_creator/install.sh
@@ -3,16 +3,18 @@ set -e
 
 mkdir -p ~/.local/share/
 sudo -A mv $(cd $(dirname $0) && pwd) ~/.local/share/user_creator
-sudo -A  chown $USER ~/.local/share/user_creator
+sudo -A  chown --recursive $USER ~/.local/share/user_creator
 sudo -A  chmod u=rwx,g=,o= ~/.local/share/user_creator
 
 cd ~/.local/share/user_creator
 
+sudo apt install -y python3-venv python3-pip python3
 rm -rf .venv || true
 python3 -m venv .venv
 source .venv/bin/activate
 pip install .
 
+mkdir ~/.local/share/applications/
 tee ~/.local/share/applications/account-creation-tool.desktop <<EOF 
 [Desktop Entry]
 Name=User creation tool
